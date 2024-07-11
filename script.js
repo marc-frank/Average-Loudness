2a93d394fe24cd112c671a071788832d4f8b6927
Marc Frank <marc.silas.frank@gmail.com>
2024-07-11 23:41:45 +0200

    Update script.js - Register service worker

--- script.js
+++ script.js
@@ -108,3 +108,16 @@ intervalSelect.addEventListener('change', (event) => {
 
 // Initialize the microphone input and graph
 getMicrophoneInput();
+
+// Register service worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+            .then(registration => {
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            })
+            .catch(error => {
+                console.log('ServiceWorker registration failed: ', error);
+            });
+    });
+}
